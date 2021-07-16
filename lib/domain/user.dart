56461860ebfbb780058cf063f5fc97599bdf9b47
Dmitry_Rachko <dima229131@gmail.com>
2021-07-16 15:47:34 +0300

    init

---
+++ lib/domain/user.dart
@@ -0,0 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Userdom {
+  String id;
+  Userdom.fromFirebase(User user) {
+    id = user.uid;
+  }
+}
---
+++ lib/root_app.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/screens/home_page.dart';
+import 'package:flutter_application_2/screens/search_page.dart';
+import 'package:flutter_application_2/theme/colors.dart';
+import 'package:cupertino_icons/cupertino_icons.dart';
+import 'package:flutter_icons/flutter_icons.dart';
+
+import 'screens/favorite_page.dart';
+
+class RootApp extends StatefulWidget {
+  @override
+  _RootAppState createState() => _RootAppState();
+}
+
+class _RootAppState extends State<RootApp> {
+  int activeTab = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: black,
+      bottomNavigationBar: getFooter(),
+      body: getBody(),
+    );
+  }
+
+  Widget getBody() {
+    return IndexedStack(
+      index: activeTab,
+      children: [
+        HomePage(),
+        SearchPage(),
+        FavoritePage(),
+        // Center(
+        //   child: Text(
+        //     "Home",
+        //     style: TextStyle(
+        //         fontSize: 20, color: white, fontWeight: FontWeight.bold),
+        //   ),
+        // ),
+        // Center(
+        //   child: Text("Serch",
+        //       style: TextStyle(
+        //           fontSize: 20, color: white, fontWeight: FontWeight.bold)),
+        // ),
+        // Center(
+        //   child: Text("Favorite",
+        //       style: TextStyle(
+        //           fontSize: 20, color: white, fontWeight: FontWeight.bold)),
+        // )
+      ],
+    );
+  }
+
+  Widget getFooter() {
+    List items = [
+      Feather.home,
+      Feather.search,
+      Feather.heart,
+    ];
+    return Container(
+        alignment: Alignment.center,
+        height: 70,
+        decoration: BoxDecoration(color: black),
+        child: Padding(
+            padding: const EdgeInsets.only(left: 30, right: 30),
+            child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: List.generate(items.length, (index) {
+                  return IconButton(
+                      icon: Icon(
+                        items[index],
+                        color: activeTab == index ? primary : white,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          activeTab = index;
+                        });
+                      });
+                }))));
+  }
+}
