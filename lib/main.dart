e402022901e8ba76a2224bb7e99be88e281e2b19
Dmitry_Rachko <dima229131@gmail.com>
2021-08-01 18:45:42 +0300

    firebase fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/my_app.dart';
-import 'package:flutter_application_2/root_app.dart';
-import 'package:flutter_application_2/screens/regisration_page.dart';
 
-void main() => runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+// void main() => runApp(MyApp());
   //   MaterialApp(
   //     debugShowCheckedModeBanner: false,
   //     home: RegisterPage(),
